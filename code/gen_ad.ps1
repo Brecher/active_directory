8ff0e1533d8c87580a37aa7843a4a067dd09a51c
Brecher <bountyhuntergarro@gmail.com>
2022-08-01 03:15:52 +0300

    Added random localadmins script

--- code/gen_ad.ps1
+++ code/gen_ad.ps1
@@ -1,40 +1,39 @@
-param( [Parameter(Mandatory=$true)] $JSONFile,
-        [switch]$Undo    
-
-)
+param( 
+    [Parameter(Mandatory=$true)] $JSONFile,
+    [switch]$Undo
+ )
 
 function CreateADGroup(){
-    param ( [Parameter(Mandatory=$true)] $groupObject )
-    
+    param( [Parameter(Mandatory=$true)] $groupObject )
+
     $name = $groupObject.name
     New-ADGroup -name $name -GroupScope Global
 }
 
 function RemoveADGroup(){
-    param ( [Parameter(Mandatory=$true)] $groupObject )
-    
+    param( [Parameter(Mandatory=$true)] $groupObject )
+
     $name = $groupObject.name
-    Remove-ADGroup -Identity $name -Confirm:$false
+    Remove-ADGroup -Identity $name -Confirm:$False
 }
 
 function CreateADUser(){
     param( [Parameter(Mandatory=$true)] $userObject )
-    
-    # Вытаскиваем имя из JSON object
+
+    # Pull out the name from the JSON object
     $name = $userObject.name
     $password = $userObject.password
 
-    # Генерируем инициалы и lastname структуры для username
+    # Generate a "first initial, last name" structure for username
     $firstname, $lastname = $name.Split(" ")
     $username = ($firstname[0] + $lastname).ToLower()
     $samAccountName = $username
     $principalname = $username
 
-
-    #Создаём AD user object
+    # Actually create the AD user object
     New-ADUser -Name "$name" -GivenName $firstname -Surname $lastname -SamAccountName $SamAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -PassThru | Enable-ADAccount
 
-    # Add the user to its group
+    # Add the user to its appropriate group
     foreach($group_name in $userObject.groups) {
 
         try {
@@ -43,15 +42,23 @@ function CreateADUser(){
         }
         catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
         {
-            Write-Warning "User $name not added to group $group_name 'coze does not exists"
+            Write-Warning "User $name NOT added to group $group_name because it does not exist"
         }
+    }
     
+    Add to local admin as needed
+    if ( $userObject.local_admin -eq $True){
+        net localgroup administrators $Global:Domain\$username /add
+    }
+    $add_command="net localgroup administrators $Global:Domain\$username /add"
+    foreach ($hostname in $userObject.local_admin){
+        echo "Invoke-Command -Computer $hostname -ScriptBlock { $add_command }" | Invoke-Expression
     }
 }
 
 function RemoveADUser(){
-    param ( [Parameter(Mandatory=$true)] $userObject )
-    
+    param( [Parameter(Mandatory=$true)] $userObject )
+
     $name = $userObject.name
     $firstname, $lastname = $name.Split(" ")
     $username = ($firstname[0] + $lastname).ToLower()
@@ -61,7 +68,7 @@ function RemoveADUser(){
 
 function WeakenPasswordPolicy(){
     secedit /export /cfg C:\Windows\Tasks\secpol.cfg
-    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0").replace("MinimumPasswordLength = 7", "MinimumPasswordLength = 1") | Out-File C:\Windows\Tasks\secpol.cfg
     secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
     rm -force C:\Windows\Tasks\secpol.cfg -confirm:$false
 }
@@ -80,20 +87,20 @@ $Global:Domain = $json.domain
 if ( -not $Undo) {
     WeakenPasswordPolicy
 
-    foreach( $group in $json.groups ){
+    foreach ( $group in $json.groups ){
         CreateADGroup $group
     }
-
-    foreach( $user in $json.users ){
+    
+    foreach ( $user in $json.users ){
         CreateADUser $user
     }
 }else{
     StrengthenPasswordPolicy
 
-    foreach ($user in $json.users ){
+    foreach ( $user in $json.users ){
         RemoveADUser $user
     }
-    foreach ($group in $json.groups ){
+    foreach ( $group in $json.groups ){
         RemoveADGroup $group
     }
 }
